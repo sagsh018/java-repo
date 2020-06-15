1d822fb58232cdc5bd6d36b9ccb47e44fbc22880
Sagar Sharma <ss0315345@outlook.com>
2020-06-15 15:07:58 +0530

    initial commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 5;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
+    }
+}
---
+++ meven/src/main/java/_some.java
@@ -0,0 +1,6 @@
+public class _some {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
