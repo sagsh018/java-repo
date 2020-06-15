ec1636606c59c499505bd21fefaae6aa7fa9d458
Sagar Sharma <ss0315345@outlook.com>
2020-06-15 21:07:29 +0530

    modifications

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    char myCharVar = 'D'; //Note that we can only store one single character in one char variable.
     }
 }
