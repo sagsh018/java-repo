8247ede3b7ea890de0e256fc7b270e5537135f56
Sagar Sharma <ss0315345@outlook.com>
2020-08-28 20:58:39 +0530

    Lecture_10

---
+++ Java_full_stack/Operators/src/ArithmaticOperators.java
@@ -0,0 +1,18 @@
+public class ArithmaticOperators {
+	public static void main(String[] args) {
+		// Below are the arithmetic operators available in java
+		// 1) + ==> adds two operands, binary operator
+		//		This is also used to concatinate two strings
+		// 2) - ==> subtract second operator from first, binary operator
+		// 3) * ==> multiply two operands, binary operator
+		// 4) / ==> divide numerator by denominator, binary operator
+		// 5) % ==> modulus operator, gives remainder, binary operator
+		// 6) ++ ==> increment operator, increases integer value by 1, unary operator
+		//		pre-increment(++x) and post increment(x++)
+		// 7) -- ==> decrement operator, decrease integer value by 1, unary operator
+		// 		pre decrement (--x) and post decrement (x--)
+		// 8) = ==> assignment operator, assign value on right to left, binary operator
+		// 9) == ==> equality operator, check left side equal to right side, binary operator
+		
+	} 
+}
