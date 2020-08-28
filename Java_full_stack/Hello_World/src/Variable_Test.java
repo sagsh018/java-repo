e2ee5028c3378a970498deeb212144ad122ef24a
Sagar Sharma <ss0315345@outlook.com>
2020-08-28 16:27:09 +0530

    Lecure_2

--- Java_full_stack/Hello_World/src/Variable_Test.java
+++ Java_full_stack/Hello_World/src/Variable_Test.java
@@ -4,6 +4,6 @@ public class Variable_Test {
 		int value1 = 10, value2 = 30, value3;
 		value3 = value1+value2;
 		System.out.println("Value : " + value3);
-		System.out.println("This line is added to change commit message for this file")
+		System.out.println("This line is added to change commit message for this file");
 	}
 }
