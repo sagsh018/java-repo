159e1b2cb1b895595f5dc03704fed6598337656a
Sagar Sharma <ss0315345@outlook.com>
2020-08-31 01:45:02 +0530

    Lecture_13

---
+++ Java_full_stack/Operators/src/AssignmentOperator.java
@@ -0,0 +1,8 @@
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		// So here we are going to take a look at = operator along with few of its variations
+		int x = 10; // This is basic assignment of value 10 to integer variable x.
+		// Now lets have a look at the variabtions as well
+		// +=, -=, *=, /=, %=
+	}
+}
