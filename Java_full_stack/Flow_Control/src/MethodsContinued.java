dc3b4e1d5be01735c0ebcd11936fae3a676afe8e
Sagar Sharma <ss0315345@outlook.com>
2020-09-01 23:30:53 +0530

    Lecture_23

---
+++ Java_full_stack/Flow_Control/src/MethodsContinued.java
@@ -0,0 +1,13 @@
+public class MethodsContinued {
+	public static void main(String[] args) {
+		/*
+		 * So here we are going to take a look at functions further and try to define and call the function with parameter as well as with some
+		 * return type other than void
+		 */
+		System.out.println("The area of rectangle is : "+areaOfRectangle(2, 3));
+	}
+	
+	public static double areaOfRectangle(double l, double b) {
+		return l*b;
+	}
+}
