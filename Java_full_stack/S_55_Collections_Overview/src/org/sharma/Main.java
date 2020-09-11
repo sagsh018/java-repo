73bdd2c8dfeab848cf962824481c52aac3b27ae7
Sagar Sharma <ss0315345@outlook.com>
2020-09-11 20:51:44 +0530

    Lecture_55

---
+++ Java_full_stack/S_55_Collections_Overview/src/org/sharma/Main.java
@@ -0,0 +1,44 @@
+package org.sharma;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * So here we are going to study about various data structures, like ArrayLists, queues, stack, set, etc.
+		 * These all are part of java.utils package
+		 * collection of all these data structures is called as Collection Framework
+		 * 
+		 * What is Collection in Java
+		 * ==========================
+		 * The Java collection framework is a collection of interfaces and classes which make it easier for storing and processing collection
+		 * of objects efficiently
+		 * 
+		 * Collection Framework
+		 * =======================
+		 * run the program below to get better picture of framework 
+		 */
+		System.out.println("\t\t\t\tCollections");
+		System.out.println("\t\t\t\t    ||");
+		System.out.println("\t\t\t\t    ||");
+		System.out.println("\t====================================================");
+		System.out.println("\t ||\t\t ||\t\t ||\t\t  ||");
+		System.out.println("\t ||\t\t ||\t\t ||\t\t  ||");
+		System.out.println("\tSet\t\tList\t\tQueue\t\tDequeue");
+		System.out.println("\t||");
+		System.out.println("\t||");
+		System.out.println("    SortedSet");
+		System.out.println("\t\t\t\tMap");
+		System.out.println("\t\t\t\t||");
+		System.out.println("\t\t\t\t||");
+		System.out.println("\t\t\t     SortedMap");
+		/*
+		 * Advantages of Collection Framework
+		 * 1) No third part API required in order to implement the data structures
+		 * 2) No need to create API from handling collection objects
+		 * 3) Collections framework is tested and optimized
+		 * 
+		 * 
+		 */
+	}
+
+}
